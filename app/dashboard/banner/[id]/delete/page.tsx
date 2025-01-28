a21fdb46c7dc64d7bbcda2b97ec6100930e8c8f2
Ghulam Raza <106047318+boythedream@users.noreply.github.com>
2025-01-28 08:17:56 +0500

    Update page.tsx

--- app/dashboard/banner/[id]/delete/page.tsx
+++ app/dashboard/banner/[id]/delete/page.tsx
@@ -1,35 +1,48 @@
 import { deleteBanner } from "@/app/actions";
 import { SubmitButton } from "@/app/components/SubmitButton";
 import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Link from "next/link";
 
-export default function DeleteBannerRoute({params}: {params: {id: string}}) {
-    return (
-        <div className="h-[80vh] flex items-center justify-center">
-        <Card className="max-w-xl">
-          <CardHeader>
-            <CardTitle>Are you absolutely sure?</CardTitle>
-            <CardDescription>
-              This action cannot be undone. This will permanently delete the
-              banner and all of its data from our server.
-            </CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <div className="flex gap-4 w-full">
-              <Button variant="outline" className="flex-1" asChild>
-                <Link href="/dashboard/banner">Cancel</Link>
-              </Button>
-              <form action={deleteBanner} className="flex-1">
-                <input type="hidden" name="bannerId" value={params.id} />
-              <SubmitButton
-              variant="destructive" className="w-full" text="Delete Banner"/>
-              </form>
-            </div>
-          </CardFooter>
-        </Card>
-      </div>
-    )
+interface PageProps {
+  params: { id: string };
 }
-            
+
+export default function DeleteBannerRoute({ params }: PageProps) {
+  return (
+    <div className="h-[80vh] flex items-center justify-center">
+      <Card className="max-w-xl">
+        <CardHeader>
+          <CardTitle>Are you absolutely sure?</CardTitle>
+          <CardDescription>
+            This action cannot be undone. This will permanently delete the
+            banner and all of its data from our server.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <div className="flex gap-4 w-full">
+            <Button variant="outline" className="flex-1" asChild>
+              <Link href="/dashboard/banner">Cancel</Link>
+            </Button>
+            <form action={deleteBanner} className="flex-1">
+              <input type="hidden" name="bannerId" value={params.id} />
+              <SubmitButton
+                variant="destructive"
+                className="w-full"
+                text="Delete Banner"
+              />
+            </form>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+        
          
